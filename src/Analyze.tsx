568c2c49cadbf826361391e39c16799dfc3b9643
Ben Grant <benjamin.j.grant@gmail.com>
2021-03-02 00:25:30 -0800

    Get MediaInfo module from blob URL (will allow reuse and progress bar on initial load)

--- src/Analyze.tsx
+++ src/Analyze.tsx
@@ -1,6 +1,9 @@
 import { h } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
 import MediaInfo from 'mediainfo.js';
+import { useRecoilState } from 'recoil';
+
+import { wasmObjectURLState } from './state';
 
 export interface AnalyzeProps {
     getSize: () => number | Promise<number>;
@@ -10,23 +13,57 @@ export interface AnalyzeProps {
 
 export default function Analyze({ getSize, readChunk, ready = true }: AnalyzeProps) {
     const [output, setOutput] = useState('');
+    const [status, setStatus] = useState('');
+    const [progress, setProgress] = useState<number | undefined>(undefined);
+    const [working, setWorking] = useState(false);
+    const [wasmObjectURL, setWasmObjectURL] = useRecoilState(wasmObjectURLState);
 
     useEffect(() => {
         if (ready) {
             (async () => {
+                setWorking(true);
+
+                let localObjectURL: string;
+
+                if (wasmObjectURL === undefined) {
+                    setStatus('Loading MediaInfo...');
+                    const wasmBlob = await (await fetch('https://unpkg.com/mediainfo.js@0.1.4/dist/MediaInfoModule.wasm')).blob();
+                    localObjectURL = URL.createObjectURL(wasmBlob);
+                    setWasmObjectURL(localObjectURL);
+                }
+
                 const mediaInfo = await MediaInfo({
-                    locateFile: () => 'https://unpkg.com/mediainfo.js@0.1.4/dist/MediaInfoModule.wasm',
+                    locateFile: () => wasmObjectURL ?? localObjectURL,
                     format: 'text',
                 });
 
-                const result = await mediaInfo.analyzeData(getSize, readChunk) as string;
+                const totalSize = await getSize();
+
+                async function reader(size: number, offset: number): Promise<Uint8Array> {
+                    setProgress(offset / totalSize);
+                    return await readChunk(size, offset);
+                }
+
+                setStatus('Processing file...');
+                const result = await mediaInfo.analyzeData(() => totalSize, reader) as string;
                 setOutput(result);
+                setWorking(false);
             })();
         }
     }, [ready]);
 
     return (
         <div class="Analyze">
+            {working && (
+                <p>
+                    {status}
+                    {typeof progress == 'number' ? (
+                        <progress value={progress}>{Math.round(progress * 100)}%</progress>
+                    ) : (
+                        <progress>(working)</progress>
+                    )}
+                </p>
+            )}
             <pre>{output}</pre>
         </div>
     );
--- src/state.ts
+++ src/state.ts
@@ -5,3 +5,23 @@ export const filesState = atom<FileList | undefined>({
     default: undefined,
     dangerouslyAllowMutability: true, // Object.freeze throws error when called on FileList
 });
+
+export const statusState = atom({
+    key: 'statusState',
+    default: '',
+});
+
+export const progressState = atom<number | undefined>({
+    key: 'progressState',
+    default: undefined,
+});
+
+export const workingState = atom({
+    key: 'workingState',
+    default: false,
+});
+
+export const wasmObjectURLState = atom<string | undefined>({
+    key: 'wasmObjectURLState',
+    default: undefined,
+});
